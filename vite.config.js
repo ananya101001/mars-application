387c90d0b402bca94b6f47988495b87bf9e25f89
Akshay Shetty <akshashe@Akshays-MacBook-Air.local>
2025-02-06 15:46:04 -0800

    Fix asset paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/mars-application/', // This is the key change
-  plugins: [react()]
+  base: '/mars-application/', // Set base path for GitHub Pages
+  plugins: [react()],
 });
