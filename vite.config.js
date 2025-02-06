165f77873ba1842722f4d4a4882313a05f7ca3c9
Akshay Shetty <akshashe@Akshays-MacBook-Air.local>
2025-02-06 15:40:26 -0800

    Update paths for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  base: '/mars-application/',
+  base: '/mars-application/', // This is the key change
   plugins: [react()]
 });
