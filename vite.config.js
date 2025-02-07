dcb7f7938127e86490ae0228c7a26279d52b4552
Akshay Shetty <akshashe@Akshays-MacBook-Air.local>
2025-02-06 16:00:48 -0800

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/mars-application/', // Set base path for GitHub Pages
   plugins: [react()],
+  base: '/mars-application/', // Ensure this matches your repo name
 });
